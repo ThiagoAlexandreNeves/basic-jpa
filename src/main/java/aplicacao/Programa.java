8cdd8650c16933f272054934c59da022a2f3d751
ThiagoAlexandreNeves <thiago,alexandre.ac@gmail.com>
2022-02-21 03:18:58 -0300

    initial commit

---
+++ src/main/java/aplicacao/Programa.java
@@ -0,0 +1,27 @@
+package aplicacao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import dominio.Pessoa;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Pessoa p = em.find(Pessoa.class, 3);
+		
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println(p);
+		
+		emf.close();
+		em.close();
+	}
+}
